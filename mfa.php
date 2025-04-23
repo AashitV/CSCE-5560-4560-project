6c57d1fd5576fad6426ae155113a92d00b865210
Lactose1738 <lucas.l.lira.02@gmail.com>
2025-04-22 21:54:05 -0500

    Update mfa.php
    Hid smtp info from public

--- mfa.php
+++ mfa.php
@@ -31,12 +31,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'OptimusFitnessOF@gmail.com';
-            $mail->Password = 'rtfp xgut ikko ycyh';
+            $mail->Username = '';
+            $mail->Password = '';
             $mail->SMTPSecure = 'tls';
             $mail->Port = 587;
 
-            $mail->setFrom('OptimusFitnessOF@gmail.com', 'OptimusFit');
+            $mail->setFrom('', 'OptimusFit');
             $mail->addAddress($user['email'], $user['username']);
             $mail->isHTML(true);
             $mail->Subject = 'Your MFA Code from OptimusFit';
